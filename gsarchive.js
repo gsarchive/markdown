608ea12a0f3bd41c52187fe988e09a08e7f28a59
Chris Angelico <rosuav@gmail.com>
2022-10-26 12:58:38 +1100

    Add styles and scripts for popup images

---
+++ gsarchive.js
@@ -0,0 +1,20 @@
+import {on, fix_dialogs} from "https://rosuav.github.io/choc/factory.js";
+const {BUTTON, DIALOG, IMG, P, SECTION} = choc; //autoimport
+ensure_popimg_dlg(); //Unnecessary overhead once Firefox 98+ is standard - can then be removed
+fix_dialogs({close_selector: ".dialog_cancel,.dialog_close", click_outside: "formless"});
+
+function ensure_popimg_dlg() {
+	//Setting the z-index is necessary only on older Firefoxes that don't support true showModal()
+	if (!DOM("#popimgdlg")) document.body.appendChild(DIALOG({id: "popimgdlg", style: "z-index: 999"}, SECTION([
+		IMG({id: "popimg_img"}),
+		P({style: "flex: 0; margin: 0"}, BUTTON({class: "dialog_close"}, "Close")),
+	])));
+}
+
+//Add class=popup to links that should pop up
+on("click", ".popup", e => {
+	ensure_popimg_dlg();
+	e.preventDefault();
+	DOM("#popimg_img").src = e.match.href;
+	DOM("#popimgdlg").showModal();
+});
